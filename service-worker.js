054b3ec92692b5016cd21f0c964beed5d162bef2
Nebu1as <nebulas.gu@ruhlamat-st.com.cn>
2020-06-11 09:18:30 +0800

    blog

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d15d91c76a6e15ac79b64d17809c0458.js"
+  "/precache-manifest.12da3626d1e1111c54ba3fc9f8fd86e6.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "unsplash"});
+workbox.core.setCacheNameDetails({prefix: "nebulas"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "unsplash"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
